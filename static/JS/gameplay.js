90ed8396459b3dad040078ee4117c150bc34774a
LinTingr <a0925391958@gmail.com>
2023-02-14 01:05:16 +0800

    add socket connection

--- static/JS/gameplay.js
+++ static/JS/gameplay.js
@@ -2,64 +2,8 @@ const canvas = document.querySelector("#canvas")
 const context  = canvas.getContext("2d");
 const ownScore = document.querySelector(".ownScore")
 const ownScorebar = document.querySelector(".ownScorebar")
-// const qqqqqq = "http://localhost:5000";
-// const socket = io();
-  
 
-class Figure {
-    constructor (xpos, ypos, radius, color, speed){
-        this.xpos = xpos;
-        this.ypos = ypos;
-        this.radius = radius;
-        this.color = color;
-        this.speed = speed;
-        this.angle = 0;
-    }
-    draw(){
-        // console.log('tank xpos:',this.xpos,'tank ypos:',this.ypos);
-        context.save()
-        context.translate(this.xpos, this.ypos);
-        context.rotate(this.angle);
-        context.beginPath();
-        context.fillStyle = "#b7b7b7";
-        //砲管
-        context.fillRect(15, -10, 30, 20);
-        context.strokeRect(15, -10, 30, 20);
-        context.strokeRectStyle = "#727272"
-        context.lineWidth = 1;
-        //人物
-        context.beginPath();
-        context.arc(0, 0, this.radius, 0, 2*Math.PI, false);
-        context.fillStyle =  this.color;
-        context.fill();
-        context.restore()
 
-        // context.stroke();
-    }
-}
-class Bullet {
-    constructor(x, y, radius, angle, speed) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius
-        this.angle = angle;
-        this.speed = speed;
-        this.damage = 1;
-    }
-    draw() {
-        context.save()
-        context.beginPath();
-        context.fillStyle = "black"
-        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-        context.fill();
-        context.restore()
-    }
-    update() {
-        this.x = this.speed.x + this.x;
-        this.y = this.speed.y + this.y;
-        this.draw()
-      }
-}
 class ScoreBall {
     constructor (x, y, radius, color){
         this.x = x;
@@ -106,133 +50,71 @@ canvas.width = window_width;
 canvas.height = window_height;
 canvas.style.background = "#cdcdcd";
 
-const figure = new Figure(canvas.width/2, canvas.height/2, 25, "mediumpurple", 2.5)
-
-let wPressed = false;
-let dPressed = false;
-let sPressed = false;
-let aPressed = false;
-
-const x = canvas.width/2
-const y = canvas.height/2
-const bullets = []
-
-const scoreTriangle =[]
-const scoreballs = [] 
+// const x = canvas.width/2
+// const y = canvas.height/2
+let players = {}
+let bullets = []
+let scoreTriangle =[]
+let scoreballs = [] 
 let score = 0 
 
-function scoreObject(){
-    setInterval(()=>{
-        if (scoreballs.length < 5) {
-            const ballX = Math.floor(Math.random() * (1200 - 20 + 1) + 20);
-            const ballY = Math.floor(Math.random() * (800 - 20 + 1) + 20);
-            // scoreTrangle.push(new ScoreTrangle())    
-            scoreballs.push(new ScoreBall(ballX, ballY, 20, "#ffe869"))
+
+socket.on("scoreball",(x)=>{
+    // console.log(x)
+    if(scoreballs.length<10){
+        for(let i = 0; i < x.length; i++){
+            scoreballs.push(new ScoreBall(x[i][0], x[i][1], 20, "#ffe869"))
         }
-        // if(scoreTriangle.length < 1){
-        //     const triangleX = Math.floor(Math.random() * (1200 - 20 + 1) + 20);
-        //     const triangleY = Math.floor(Math.random() * (800 - 20 + 1) + 20);
-        //     scoreTriangle.push(new ScoreTriangle(triangleX, triangleY, 100, "#fc7677"))
-        // }
-    },1000) 
-}
+    }
+})
+// function scoreObject(X){
+//     setInterval(()=>{
+//         if (scoreballs.length < 5) {
+//             const ballX = Math.floor(Math.random() * (1200 - 20 + 1) + 20);
+//             const ballY = Math.floor(Math.random() * (800 - 20 + 1) + 20);
+//             // scoreTrangle.push(new ScoreTrangle())    
+//             scoreballs.push(new ScoreBall(ballX, ballY, 20, "#ffe869"))
+//         }
+//         // if(scoreTriangle.length < 1){
+//         //     const triangleX = Math.floor(Math.random() * (1200 - 20 + 1) + 20);
+//         //     const triangleY = Math.floor(Math.random() * (800 - 20 + 1) + 20);
+//         //     scoreTriangle.push(new ScoreTriangle(triangleX, triangleY, 100, "#fc7677"))
+//         // }
+//     },1000) 
+// }
 
 function projectile(){
-    // let isClicked = false;
-    // let ckc = 0 
     addEventListener("click", (event)=>{ 
-        // if (isClicked) return;
-        // isClicked = true;
-        // if(ckc == 0){
-        //     let rect = canvas.getBoundingClientRect();
-        //     let xx = event.clientX - rect.left;
-        //     let yy = event.clientY - rect.top;
-        //     let angle = Math.atan2(yy - figure.ypos, xx - figure.xpos);
-        //     let bbbb = new Bullet(figure.xpos+50*Math.cos(angle), figure.ypos+50*Math.sin(angle), 10, angle, {x:Math.cos(angle)*3,y:Math.sin(angle)*3});
-        //     bullets.push(bbbb);
-        // }
-        // ckc += 1
-        // setTimeout(()=>{
-            let rect = canvas.getBoundingClientRect();
-            let xx = event.clientX - rect.left;
-            let yy = event.clientY - rect.top;
-            let angle = Math.atan2(yy - figure.ypos, xx - figure.xpos);
-            let bbbb = new Bullet(figure.xpos+50*Math.cos(angle), figure.ypos+50*Math.sin(angle), 10, angle, {x:Math.cos(angle)*3,y:Math.sin(angle)*3});
-            bullets.push(bbbb);
-            isClicked = false;
-        // },500)
+        let rect = canvas.getBoundingClientRect();
+        let xx = event.clientX - rect.left;
+        let yy = event.clientY - rect.top;
+        let angle = Math.atan2(yy - figure.ypos, xx - figure.xpos);
+        let bullet = new Bullet(
+            figure.xpos+50*Math.cos(angle), 
+            figure.ypos+50*Math.sin(angle), 
+            10, 
+            angle, 
+            {x:Math.cos(angle)*6,y:Math.sin(angle)*6});
+        socket.emit("bullets", bullet)
+        bullets.push(bullet);
+        console.log(bullets)
     }) 
 }
 
 
-function figuremove(){
-    document.addEventListener("keydown", function(event) {
-        if(event.keyCode === 87) {
-            // console.log("w", wPressed)
-            wPressed = true;
-        }
-        if(event.keyCode === 68) {
-            // console.log("d", dPressed)
-            dPressed = true;
-        }
-        if(event.keyCode === 83) {
-            // console.log("s", sPressed)
-            sPressed = true;
-        }
-        if(event.keyCode === 65) {
-            // console.log("a", aPressed)
-            aPressed = true;
-        }
-    });
-    document.addEventListener("keyup", function(event) {
-        if(event.keyCode === 87) {
-            // console.log("w", wPressed)
-            wPressed = false;
-        }
-        if(event.keyCode === 68) {
-            // console.log("d", dPressed)
-            dPressed = false;
-        }
-        if(event.keyCode === 83) {
-            // console.log("s", sPressed)
-            sPressed = false;
-        }
-        if(event.keyCode === 65) {
-            // console.log("a", aPressed)
-            aPressed = false;
-        }
-    });
-    if (aPressed){ // left key:a
-        if(figure.xpos > figure.radius) {
-            figure.xpos -= figure.speed;
-        }
-        if (figure.xpos - figure.radius <= 0) {
-            figure.xpos = figure.radius;
-        }
+
+function palyermove(){
+    if (aPressed) { 
+        figure.xpos = (figure.xpos > figure.radius) ? figure.xpos - figure.speed : figure.radius;
     } 
-    if (wPressed){ // up key:w
-        if(figure.ypos > figure.radius) {
-            figure.ypos -= figure.speed;
-        }
-        if (figure.ypos - figure.radius <= 0) {
-            figure.ypos = figure.radius;
-        }
+    if (wPressed) { 
+        figure.ypos = (figure.ypos > figure.radius) ? figure.ypos - figure.speed : figure.radius;
     }     
-    if (dPressed){ // right key:d
-        if(figure.xpos < canvas.width - figure.radius) {
-            figure.xpos += figure.speed;
-        }
-        if (figure.xpos + figure.radius >= canvas.width) {
-            figure.xpos = canvas.width - figure.radius;
-        }
+    if (dPressed) { 
+        figure.xpos = (figure.xpos < canvas.width - figure.radius) ? figure.xpos + figure.speed : canvas.width - figure.radius;
     }     
-    if (sPressed){ // down key:s
-        if(figure.ypos < canvas.height - figure.radius) {
-            figure.ypos += figure.speed;
-        }
-        if (figure.ypos + figure.radius >= canvas.height) {
-            figure.ypos = canvas.height - figure.radius;
-        }
+    if (sPressed) { 
+        figure.ypos = (figure.ypos < canvas.height - figure.radius) ? figure.ypos + figure.speed : canvas.height - figure.radius;
     }  
 }
 
@@ -244,20 +126,35 @@ function mousemove(){
         let y = event.clientY - rect.top;
         angle = Math.atan2(y - figure.ypos, x - figure.xpos);
         // 計算物件與滑鼠之間的角度
-        // context.translate(figure.xpos, figure.ypos);
         figure.angle = angle; // 更新物件角度
     });
 }
-function startgame(){
-    // console.log("tank x:",figure.xpos,"  tank y:",figure.ypos)
-    figuremove();
-    mousemove();
 
+let abc 
+socket.on("update", (playinfo)=>{
+    context.clearRect(0, 0, canvas.width, canvas.height);       
+    for(let id in playinfo){
+            if(id !== socket.id){
+                players[id] = new Figure(playinfo[id].name, playinfo[id].x, playinfo[id].y, 25, "mediumpurple", 2.5, playinfo[id].angle)
+                abc = players[id]
+            }
+        }
+})
+socket.on("bullet", (otherbullets)=>{
+    let bullet = new Bullet(otherbullets.x, otherbullets.y, otherbullets.radius, otherbullets.angle, otherbullets.speed)
+    bullets.push(bullet)
+})
+function startgame(){
     context.clearRect(0, 0, canvas.width, canvas.height);
+
 //////////////////////////////////////////////////////////////////////////////////////
     bullets.forEach(function(bullet){
         bullet.update();
     })
+    // otherbullets.forEach(function(bullet){
+    //     console.log("otherbullets", otherbullets)
+    //     bullet.update();
+    // })
     scoreTriangle.forEach(function(triangle, triangleindex){
         triangle.update();
         const distToTriangleCenter = Math.hypot(figure.xpos - triangle.x, figure.ypos - triangle.y);
@@ -286,6 +183,7 @@ function startgame(){
             if (dist1 < triangle.size/2 || dist2 < triangle.size/2 || dist3 < triangle.size/2){
                 bullets.splice(bulletindex, 1)
                 scoreTriangle.splice(triangleindex, 1)
+                socket.emit("ballclear", scoreballs)
             }
         })
     })
@@ -318,41 +216,32 @@ function startgame(){
             }
         })
     })
-    
-    figure.draw(context);
 
+    figure.draw()
+    // 傳資料到伺服端
+    socket.emit("playinit", {
+        name:user.username,
+        x: figure.xpos,
+        y: figure.ypos, 
+        radius:25, 
+        color:"black", 
+        speed:2.5, 
+        angle:figure.angle
+    }) 
+    
+
+    // 資料傳到客戶端
+    if(typeof(abc)=="object"){
+        abc.draw()
+    }
+
+    palyermove();
+    mousemove();
     requestAnimationFrame(startgame);
 }
 
-startgame()
-projectile()
-scoreObject()
-
-// function anime(){
-//     requestAnimationFrame(anime);
-//     context.clearRect(0, 0, canvas.width, canvas.height);
-
-// }
-
-
-// anime();
-
-
-
-
-
-//////////////////
-// canvas.addEventListener("mousemove", function(event) {
-//     context.save();
-//     let rect = this.getBoundingClientRect();
-//     let mouseX = event.clientX - rect.left;
-//     let mouseY = event.clientY - rect.top;
-//     let  angle = Math.atan2(mouseY -  figure.ypos, mouseX - figure.xpos);
-//     context.clearRect(0, 0, canvas.width, canvas.height);
-//     context.translate(figure.xpos, figure.ypos);
-//     context.rotate(angle);
-//     //畫圖 
-//     figure.draw(context);
-//     context.restore();
-//     // console.log("Mouse is at x: " + x + ", y: " + y + " inside the canvas");
-// });
+// socket.on("bullet", (bullet)=>{
+//     console.log(bullet)
+//     // let bbbb = new Bullet(abc.xpos+50*Math.cos(abc.angle), abc.ypos+50*Math.sin(abc.angle), 10, abc.angle, {x:Math.cos(abc.angle)*6,y:Math.sin(abc.angle)*6});
+//     bullets.push(bullet);
+// })
