c0f5e8a9bcdbe9ac2e7fe36014cbc133a2fc1ba1
LinTingr <a0925391958@gmail.com>
2023-02-20 22:28:22 +0800

    add player booldBar and gameover

--- static/JS/player.js
+++ static/JS/player.js
@@ -1,5 +1,5 @@
 class Figure {
-    constructor (name, xpos, ypos, radius, color, speed, angle){
+    constructor (name, xpos, ypos, radius, color, speed, angle, hp, damage, score, exist){
         this.xpos = xpos;
         this.name = name
         this.ypos = ypos;
@@ -7,17 +7,21 @@ class Figure {
         this.color = color;
         this.speed = speed;
         this.angle = angle;
+        this.hp = hp;
+        this.damage = damage;
+        this.score = score;
+        this.exist = exist;
     }
     draw(){
         context.save()
         context.translate(this.xpos, this.ypos);
         context.rotate(this.angle);
         context.beginPath();
-        context.fillStyle = "#b7b7b7";
         //砲管
+        context.fillStyle = "#b7b7b7";
         context.fillRect(15, -10, 30, 20);
-        context.strokeRect(15, -10, 30, 20);
         context.strokeRectStyle = "#727272"
+        context.strokeRect(15, -10, 30, 20);
         context.lineWidth = 1;
         //人物
         context.beginPath();
@@ -25,10 +29,14 @@ class Figure {
         context.fillStyle =  this.color;
         context.fill();
         context.restore()
+        //血條
+        context.fillStyle = "#ff0000";
+        context.fillRect(this.xpos-30, this.ypos+30, 50 * (this.hp / 100), 10);
+        context.strokeRect(this.xpos-30, this.ypos+30, 50* (120 / 100), 10);
         //名字
-        context.fillStyle = "rgba(255, 255, 255, 0.5)";
-        context.font = "bold 18px sans-serif";
-        context.fillText(this.name, this.xpos-16, this.ypos+40);
+        // context.fillStyle = "rgba(255, 255, 255, 0.5)";
+        // context.font = "bold 18px sans-serif";
+        // context.fillText(this.name, this.xpos-16, this.ypos+40);
     }
 }
 
@@ -53,12 +61,12 @@ class Bullet {
     update() {
         this.x = this.speed.x + this.x;
         this.y = this.speed.y + this.y;
-        // 子彈超出邊界後消失
-        if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
-            let index = bullets.indexOf(this);
-            bullets.splice(index, 1);
-            return;
-        }
+        // // 子彈超出邊界後消失
+        // if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) {
+        //     let index = bullets.indexOf(this);
+        //     bullets.splice(index, 1);
+        //     return;
+        // }
         this.draw()
     }
 }
@@ -104,3 +112,9 @@ document.addEventListener("keyup", function(event) {
         aPressed = false;
     }
 });
+
+function gameover(){
+    context.fillStyle = "rgba(0, 205, 205, 0.8)";
+    context.font = "bold 48px sans-serif";
+    context.fillText("- Game Over -", canvas.width/2-180, canvas.height/2);
+}
